a50586700361ddeee5ef1638a68b004e084c4bba
308k <18987735867@163.com>
2023-08-27 15:50:17 +0800

    12

--- 1.js
+++ 1.js
@@ -1,12 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var textarea = document.getElementById("textarea1");
+    var textarea1 = document.getElementById("textarea1");
+    var textarea2 = document.getElementById("textarea2");
 
     // 获取 URL 中的锚点部分
     var hash = window.location.hash;
 
-    // 去除 # 号并填入 textarea
+    // 去除 # 号并填入对应的 textarea
     if (hash) {
         var decodedText = decodeURIComponent(hash.substring(1)); // 去除 # 号并解码
         textarea1.value = decodedText;
+        textarea2.value = decodedText;
     }
 });
